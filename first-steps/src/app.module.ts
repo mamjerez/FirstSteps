1d45e3b6adc5bfa24be1f6f0d829f09293061c58
Miguel Angel Martinez <correomam@gmail.com>
2021-01-24 18:46:18 +0100

    Primer commit

---
+++ first-steps/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PhotoModule } from './photo.module';
+
+@Module({
+  imports: [
+    PhotoModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
